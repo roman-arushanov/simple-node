353faf7e278906e9724a1ddcb6493f8dfa5ad9a4
roman-arushanov <roman.arushanov@gmail.com>
2022-07-03 11:32:22 +0400

    trying to deploy

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
-app.get ('/', (req, res) => res.send('Hello Lyovik'));
+app.get ('/', (req, res) => res.send(' Hello Lyovik'));
 app.listen (port, () => console.log('Server is rungin on port ' + port + '. You need to run too '));
