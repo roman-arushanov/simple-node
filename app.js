3612b5ac9facd619a264b661e9d219ba26b40acd
roman-arushanov <roman.arushanov@gmail.com>
2022-07-03 11:26:02 +0400

    abc

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const express = require('express');
 const app = express();
 var port = process.env.PORT || 3000;
 app.get ('/', (req, res) => res.send('Hello Lyovik'));
-app.listen (port, () => console.log('Server is rungin on port ' + port + '. You need to run too'));
+app.listen (port, () => console.log('Server is rungin on port ' + port + '. You need to run too '));
